326fb1b35bfe81cb3ce4c568ed5392339149379b
George Fouche <fouliex@gmail.com>
2016-12-19 12:45:47 -0800

    Update and cleanup LetterChanges problem

--- src/StringManipulation/LetterChanges.java
+++ src/StringManipulation/LetterChanges.java
@@ -1,9 +1,11 @@
 package StringManipulation;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
+ * Using the Java language, have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm.
+ * Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a).
+ * Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string."
  * Created by fouli on 12/14/2016.
  */
 public class LetterChanges {
@@ -72,9 +74,4 @@ public class LetterChanges {
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        char a = 'a';
-        a = (char) (a + 1);
-        System.out.println(a);
-    }
 }
